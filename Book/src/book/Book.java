fb4b139c0c76dffb230a309ac451b62117f68862
Lianhahaha <itsmecalcheese@gmail.com>
2025-02-20 18:25:27 +0800

    Update Book.java

--- Book/src/book/Book.java
+++ Book/src/book/Book.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package book;
 
 /**
@@ -13,19 +9,19 @@ public class Book {
     private String author;
     private int copies;
 
-    // Constructor to initialize the book details
+
     public Book(String title, String author, int copies) {
         this.title = title;
         this.author = author;
         this.copies = copies;
     }
 
-    // Method to display book information
+    
     public void displayInfo() {
         System.out.println("Title: " + title + ", Author: " + author + ", Copies: " + copies);
     }
 
-    // Getter and Setter methods
+    
     public String getTitle() {
         return title;
     }
